d4b65bd0a37eb6729dfe4b8223bfa2a6a489fb02
Colin Pollard <colinpollard3@gmail.com>
2018-09-02 20:50:23 +0100

    db credentials

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,8 @@
 const dbSettings = (process.env.NODE_ENV == "production") ? {
-    "host": "185.216.77.171",
-    "user": "cpad_user",
-    "password": "c5v36a7ajf4guhx6",
-    "database": "cp_address_book"
+    "host": process.env.DB_HOST,
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DEFAULT
 } : {
     "host": "localhost",
     "user": "root",
